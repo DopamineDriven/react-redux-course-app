6665fd8d4ab20c9a841fc62ddb17fcf52a05f7ce
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-20 02:29:45 -0500

    Router and App wrapped in ReduxProvider for global store access

--- src/index.jsx
+++ src/index.jsx
@@ -4,10 +4,31 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App.jsx";
 import './index.css';
+import configureStore from './redux/configureStore.jsx';
+import { Provider as ReduxProvider } from 'react-redux';
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+    <ReduxProvider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </ReduxProvider>,
   document.getElementById("app")
 );
+
+// NOTE
+    // can be useful to pass initial state into configureStore() if:
+        // server rendering or
+        // initializing redux store with data from localStorage
+    // passing state here overrides default parameters specified in reducers
+    // USE when wanting to rehydrate store using a separate state passed down
+    // from the server or stored in localStorage
+
+// Provider
+    // higher order component that provides redux store data to child components
+        // alias as ReduxProvider for syntactical clarity
+    // pass store to provider on props
+    // ENTIRE app wrapped in provider component (router and all)
+        // allows app to access store globally
