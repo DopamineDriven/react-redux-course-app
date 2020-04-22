dea65377fc6934ae8f0286ecffdc694ddf6125d5
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-22 09:52:43 -0500

    created courseform, selectinput, and textinput components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ import Container from './container/Container.jsx'
 import Wrapper from './wrapper/Wrapper.jsx';
 import Footer from './footer/Footer.jsx';
 import Navbar from './navbar/Navbar.jsx';
+import ManageCoursePage from './courses/ManageCoursePage.jsx';
 
 
 const App = () => {
@@ -23,6 +24,8 @@ const App = () => {
                     <Route exact path="/" component={HomePage} />
                     <Route path="/about" component={AboutPage} />
                     <Route path="/courses" component={CoursesPage} />
+                    <Route path="/course/:slug" component={ManageCoursePage} />
+                    <Route path="/course" component={ManageCoursePage} />
                     <Route component={PageNotFound} />
                 </Switch>
             </Container>
@@ -33,3 +36,10 @@ const App = () => {
 }
 
 export default App;
+
+// /course/:slug
+    // can be thought of as a url friendly id
+    // like an id, the slug is unique 
+// ensure that the slug route (/course/:slug) is declared first
+    // only one route in switch can match so order is important
+    // if /course were declared first this method would not function 
--- src/components/courses/ManageCoursePage.jsx
+++ src/components/courses/ManageCoursePage.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { loadCourses } from '../../redux/actions/courseActions.jsx';
+import { loadAuthors } from '../../redux/actions/authorActions.jsx';
+import PropTypes from 'prop-types';
 
-const ManageCoursePage = () => {
+
+function ManageCoursePage ({ courses, authors, loadAuthors, loadCourses }) {
+
+    useEffect(() => {
+        if (courses.length === 0) {
+            loadCourses().catch(error => {
+                alert(`loading courses failed ${error}`)
+            })
+        }
+        if (authors.length === 0) {
+            loadAuthors().catch(error => {
+                alert(`loading authors failed ${error}`)
+            })
+        }
+    }, [])
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <h2>Manage Course</h2>
+        </>
     )
 }
 
-export default ManageCoursePage
+ManageCoursePage.propTypes = {
+    authors: PropTypes.array.isRequired,
+    courses: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired,
+    loadCourses: PropTypes.func.isRequired,
+    loadAuthors: PropTypes.func.isRequired
+};
+
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses,
+        authors: state.authors
+    }
+}
+
+
+// (a)
+const mapDispatchToProps = {
+    loadCourses,
+    loadAuthors
+}
+
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps)
+export default connectedStateAndProps(ManageCoursePage);
+
+
+/*
+(a)
+declare mapDispatchToProps as an object instead of a function
+call loadCourses and loadAuthors as named import
+    The bound action passed on props takes precedence over the module scope
+    that is the function scope takes precedence over the module scope 
+*/
