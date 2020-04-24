10a154fe0066edaae184dd299276b92238c7adbd
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-04-24 05:17:53 -0500

    configuring jest

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+it("should pass", () => {
+    expect(true).toEqual(true)
+});
+
+
+
+// automatically finds files ending in .test.js or .spec.js
+// automatically run jest on save with --watch in script in package.json
+    // if --watch doesn't work, use --watchAll
